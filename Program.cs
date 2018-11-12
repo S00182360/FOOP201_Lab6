ca15d28ffc41aa177ab353cf8219580c6924d418
Sam Colville <scolville47@gmail.com>
2018-11-12 12:42:43 +0000

    Added base class and child class Bill Override ToSting() Use GetType()

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace FOOP201_Lab6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Bill bill1 = new Bill("Sam", 35.99m);
+            Bill bill2 = new Bill("John", 29.99m);
+            Console.WriteLine(bill1.ToString());
+            Console.WriteLine(bill2.ToString());
+
+            OverDueBill bill3 = new OverDueBill("Ronan", 86.99m, 5);
+            Console.WriteLine(bill3);
+        }
+    }
+
+    class Bill
+    {
+        public Bill(string name, decimal amount)
+        {
+            Name = name;
+            Amount = amount;
+        }
+        public Bill()
+        {
+        }
+        public string Name
+        {
+            get;
+            set; 
+        }
+        public decimal Amount
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\nName: {1}\nAmount: {2}", GetType(), Name, Amount);
+        }
+    }
+
+    class OverDueBill : Bill
+    {
+        public OverDueBill(string name, decimal amount) : base(name, amount)
+        {
+        }
+        public OverDueBill(string name, decimal amount, int days) : base(name, amount)
+        {
+            Days = days;
+        }
+        public int Days
+        {
+            get;
+            set;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "\nDays Overdue: " + Days;
+        }
+    }
+
+}
